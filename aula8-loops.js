28110b6cdae7a1b9397131e0996c387868ecaaef
rafael <rafael.barbosa@unimedrecife.com.br>
2024-06-26 16:11:53 -0300

    aula8

---
+++ aula8-loops.js
@@ -0,0 +1,27 @@
+const listaDeDestinos = new Array(
+    'São Paulo',
+    'Rio de Janeiro',
+    'salvador',
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Salvador";
+
+console.log(listaDeDestinos);
+
+const podeComprar =  idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+let destinoExiste = false;
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino exite");
+        break;
+    }else{
+        destinoExiste = false;
+    }
+    ConstantSourceNode += 1;
+}
+console.log("Destino existe: ", destinoExiste);
