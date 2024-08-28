2056070ebb64180c77c82e12ff2aaa482dfc694d
rafael <rafael.barbosa@unimedrecife.com.br>
2024-08-28 13:23:38 -0300

    adicionando POO

---
+++ aula9-POO.js
@@ -0,0 +1,23 @@
+
+class Pessoa {
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    dizerOla() {
+        console.log(`Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos.`);
+    }
+
+    fazerAniversario() {
+        this.idade += 1;
+        console.log(`Parabéns ${this.nome}, agora você tem ${this.idade} anos!`);
+    }
+}
+
+const pessoa1 = new Pessoa('Maria', 30);
+const pessoa2 = new Pessoa('rafale', 21)
+
+pessoa1.dizerOla(); 
+pessoa2.dizerOla();
+pessoa1.fazerAniversario();
